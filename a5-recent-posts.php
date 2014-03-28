8a95ae3c22727c508dcf9d2fa26a23b240677f48
tepelstreel <tepelstreel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-28 10:01:50 +0000

    Recent Post Widget 2.2 tag
    git-svn-id: https://plugins.svn.wordpress.org/a5-recent-posts/tags/2.2@883470 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- a5-recent-posts.php
+++ a5-recent-posts.php
@@ -3,7 +3,7 @@
 Plugin Name: A5 Recent Post Widget
 Plugin URI: http://wasistlos.waldemarstoffel.com/plugins-fur-wordpress/recent-post-widget
 Description: A5 Recent Posts Widget just displays the most recent post in a customizable widget. Set the colours of the links and border, show the widget on sites, that you define, ready.
-Version: 2.1
+Version: 2.2
 Author: Waldemar Stoffel
 Author URI: http://www.waldemarstoffel.com
 License: GPL3
@@ -30,16 +30,22 @@ Text Domain: a5-recent-posts
 
 /* Stop direct call */
 
-if(preg_match('#' . basename(__FILE__) . '#', $_SERVER['PHP_SELF'])) die('Sorry, you don\'t have direct access to this page.');
+defined('ABSPATH') OR exit;
 
-define( 'RPW_PATH', plugin_dir_path(__FILE__) );
+if (!defined('RPW_PATH')) define( 'RPW_PATH', plugin_dir_path(__FILE__) );
+if (!defined('RPW_BASE')) define( 'RPW_BASE', plugin_basename(__FILE__) );
 
+# loading the framework
 if (!class_exists('A5_Image')) require_once RPW_PATH.'class-lib/A5_ImageClass.php';
 if (!class_exists('A5_Excerpt')) require_once RPW_PATH.'class-lib/A5_ExcerptClass.php';
-if (!class_exists('A5_Recent_Post_Widget')) require_once RPW_PATH.'class-lib/RPW_WidgetClass.php';
 if (!class_exists('A5_FormField')) require_once RPW_PATH.'class-lib/A5_FormFieldClass.php';
 if (!class_exists('A5_OptionPage')) require_once RPW_PATH.'class-lib/A5_OptionPageClass.php';
-if (!class_exists('A5_DynamicCSS')) require_once RPW_PATH.'class-lib/A5_DynamicCSSClass.php';
+if (!class_exists('A5_DynamicFiles')) require_once RPW_PATH.'class-lib/A5_DynamicFileClass.php';
+
+#loading plugin specific classes
+if (!class_exists('RPW_Admin')) require_once RPW_PATH.'class-lib/RPW_AdminClass.php';
+if (!class_exists('RPW_DynamicCSS')) require_once RPW_PATH.'class-lib/RPW_DynamicCSSClass.php';
+if (!class_exists('A5_Recent_Post_Widget')) require_once RPW_PATH.'class-lib/RPW_WidgetClass.php';
 
 class RecentPostWidget {
 
@@ -51,52 +57,21 @@ class RecentPostWidget {
 		
 		self::$options = get_option('rpw_options');
 		
-		register_activation_hook(  __FILE__, array($this, 'install') );
-		register_deactivation_hook(  __FILE__, array($this, 'uninstall') );
+		if (isset(self::$options['tags'])) $this->update_plugin_options();
 		
-		add_action('admin_enqueue_scripts', array($this, 'enqueue_scripts'));
-		add_filter('plugin_row_meta', array($this, 'register_links'), 10, 2);
-		add_filter('plugin_action_links', array($this, 'register_action_links'), 10, 2);
+		register_activation_hook(  __FILE__, array(&$this, 'install') );
+		register_deactivation_hook(  __FILE__, array(&$this, 'uninstall') );
 		
-		add_action('admin_init', array($this, 'register_settings'));
-		add_action('admin_menu', array($this, 'add_settings_page'));
+		add_action('admin_enqueue_scripts', array(&$this, 'enqueue_scripts'));
+		add_filter('plugin_row_meta', array(&$this, 'register_links'), 10, 2);
+		add_filter('plugin_action_links', array(&$this, 'register_action_links'), 10, 2);
 		
 		// import laguage files
 
 		load_plugin_textdomain(self::language_file, false , basename(dirname(__FILE__)).'/languages');
 		
-		$dynamic_css = new A5_DynamicCSS;
-		
-		$eol = "\r\n";
-		$tab = "\t";
-		
-		$css_selector = '.widget_a5_recent_post_widget[id^="a5_recent_post_widget"]';
-		
-		A5_DynamicCSS::$styles .= $eol.'/* CSS portion of the A5 Recent Post Widget */'.$eol.$eol;
-		
-		$style = '-moz-hyphens: auto;'.$eol.$tab.'-o-hyphens: auto;'.$eol.$tab.'-webkit-hyphens: auto;'.$eol.$tab.'-ms-hyphens: auto;'.$eol.$tab.'hyphens: auto;';
-		
-		if (!empty(self::$options['rpw_css'])) $style.=$eol.$tab.str_replace('; ', ';'.$eol.$tab, str_replace(array("\r\n", "\n", "\r"), ' ', self::$options['rpw_css']));
-		
-		A5_DynamicCSS::$styles.='div'.$css_selector.','.$eol.'li'.$css_selector.','.$eol.'aside'.$css_selector.' {'.$eol.$tab.$style.$eol.'}'.$eol;
-		
-		A5_DynamicCSS::$styles.='div'.$css_selector.' img,'.$eol.'li'.$css_selector.' img,'.$eol.'aside'.$css_selector.' img {'.$eol.$tab.'height: auto;'.$eol.$tab.'max-width: 100%;'.$eol.'}'.$eol;
-		
-		if (!empty (self::$options['link'])) :
-		
-			$style=str_replace('; ', ';'.$eol.$tab, str_replace(array("\r\n", "\n", "\r"), ' ', self::$options['link']));
-		
-			A5_DynamicCSS::$styles.='div'.$css_selector.' a,'.$eol.'li'.$css_selector.' a,'.$eol.'aside'.$css_selector.' a {'.$eol.$tab.$style.$eol.'}'.$eol;
-			
-		endif;
-		
-		if (!empty (self::$options['hover'])) :
-		
-			$style=str_replace('; ', ';'.$eol.$tab, str_replace(array("\r\n", "\n", "\r"), ' ', self::$options['hover']));
-		
-			A5_DynamicCSS::$styles.='div'.$css_selector.' a:hover,'.$eol.'li'.$css_selector.' a:hover,'.$eol.'aside'.$css_selector.' a:hover {'.$eol.$tab.$style.$eol.'}'.$eol;
-			
-		endif;
+		$RPW_DynamicCSS = new RPW_DynamicCSS;
+		$RPW_Admin = new RPW_Admin;
 		
 	}
 
@@ -115,8 +90,7 @@ class RecentPostWidget {
 	
 	function register_links($links, $file) {
 		
-		$base = plugin_basename(__FILE__);
-		if ($file == $base) {
+		if ($file == RPW_BASE) {
 			$links[] = '<a href="http://wordpress.org/extend/plugins/a5-recent-posts/faq/" target="_blank">'.__('FAQ', self::language_file).'</a>';
 			$links[] = '<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=YGA57UKZQVP4A" target="_blank">'.__('Donate', self::language_file).'</a>';
 		}
@@ -126,62 +100,10 @@ class RecentPostWidget {
 	
 	function register_action_links( $links, $file ) {
 		
-		$base = plugin_basename(__FILE__);
-		
-		if ($file == $base) array_unshift($links, '<a href="'.admin_url( 'options-general.php?page=a5-recent-posts-settings' ).'">'.__('Settings', self::language_file).'</a>');
+		if ($file == RPW_BASE) array_unshift($links, '<a href="'.admin_url( 'options-general.php?page=a5-recent-posts-settings' ).'">'.__('Settings', self::language_file).'</a>');
 	
 		return $links;
 	
-	}
-	
-	// init
-	
-	function register_settings() {
-		
-		register_setting( 'rpw_options', 'rpw_options', array($this, 'validate') );
-		
-		add_settings_section('rpw_settings', __('Styling of the links', self::language_file), array($this, 'display_section'), 'rpw_styles');
-		
-		add_settings_field('rpw_link_style', __('Link style:', self::language_file), array($this, 'link_field'), 'rpw_styles', 'rpw_settings');
-		
-		add_settings_field('rpw_hover_style', __('Hover style:', self::language_file), array($this, 'hover_field'), 'rpw_styles', 'rpw_settings');
-		
-		add_settings_field('use_own_css', __('Widget container:', self::language_file), array($this, 'rpw_display_css'), 'rpw_styles', 'rpw_settings', array(__('You can enter your own style for the widgets here. This will overwrite the styles of your theme.', self::language_file), __('If you leave this empty, you can still style every instance of the widget individually.', self::language_file)));
-		
-		add_settings_field('rpw_resize', false, array($this, 'resize_field'), 'rpw_styles', 'rpw_settings');
-	
-	}
-	
-	function display_section() {
-		
-		echo '<p>'.__('Just put some css code here.', self::language_file).'</p>';
-	
-	}
-	
-	function link_field() {
-		
-		a5_textarea('link', 'rpw_options[link]', @self::$options['link'], false, array('cols' => 35, 'rows' => 3));
-		
-	}
-	
-	function hover_field() {
-		
-		a5_textarea('hover', 'rpw_options[hover]', @self::$options['hover'], false, array('cols' => 35, 'rows' => 3));
-		
-	}
-	
-	function rpw_display_css($labels) {
-		
-		echo $labels[0].'</br>'.$labels[1].'</br>';
-		
-		a5_textarea('rpw_css', 'rpw_options[rpw_css]', @self::$options['rpw_css'], false, array('rows' => 10, 'cols' => 35));
-		
-	}
-	
-	function resize_field() {
-		
-		a5_resize_textarea(array('link', 'hover', 'rpw_css'), true);
-		
 	}
 	
 	// Creating default options on activation
@@ -189,8 +111,8 @@ class RecentPostWidget {
 	function install() {
 		
 		$default = array(
-			'tags' => array(),
-			'sizes' => array()
+			'cache' => array(),
+			'inline' => NULL
 		);
 		
 		add_option('rpw_options', $default);
@@ -205,52 +127,22 @@ class RecentPostWidget {
 		
 	}
 	
-	// Installing options page
+	// updating options in case they are outdated
 	
-	function add_settings_page() {
+	function update_plugin_options() {	
 		
-		$pages=add_options_page('A5 Recent Post '.__('Settings', self::language_file), '<img alt="" src="'.plugins_url('a5-recent-posts/img/a5-icon-11.png').'"> A5 Recent Posts', 'administrator', 'a5-recent-posts-settings', array($this, 'build_options_page'));
-		
-	}
-	
-	// Calling the options page
-	
-	function build_options_page() {
-		
-		A5_OptionPage::open_page('A5 Recent Post Widget', __('http://wasistlos.waldemarstoffel.com/plugins-fur-wordpress/recent-post-widget', self::language_file), 'a5-recent-posts', __('Plugin Support', self::language_file));
-		
-		_e('Style the links of the widget. If you leave this empty, your theme will style the hyperlinks.', self::language_file); ?>
-		<p><?php _e('Just input something like,', self::language_file); ?></p>
-		<p><strong>font-weight: bold;<br />
-		color: #0000ff;<br />
-		text-decoration: underline;    
-		</strong></p>
-		<?php _e('to get fat, blue, underlined links.', self::language_file); ?></p>
-		<p><strong><?php _e('You most probably have to use &#34;!important&#34; at the end of each line, to make it work.', self::language_file); ?></strong></p>
-		<?php
-		
-		A5_OptionPage::open_form('options.php');
-		
-		settings_fields('rpw_options');
-		do_settings_sections('rpw_styles');
-		submit_button();
-		
-		A5_OptionPage::close_page();
-		
-	}
-	
-	function validate($input) {
-		
-		self::$options['link']=trim($input['link']);
-		self::$options['hover']=trim($input['hover']);
-		self::$options['rpw_css']=trim($input['rpw_css']);
-		
-		return self::$options;
+			self::$options['cache'] = array();
+			
+			self::$options['inline'] = NULL;
+			
+			unset(self::$options['tags'], self::$options['sizes']);
+			
+			update_option('rpw_options', self::$options);
 	
 	}
 
 } // end of class
 
-$a5_recent_post_widget = new RecentPostWidget;
+$A5_Recent_Post_Widget = new RecentPostWidget;
 
 ?>
